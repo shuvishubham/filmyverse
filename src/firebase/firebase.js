5732b67dbc6b11632099d8ac07553ced8a5a5b9d
Anonyomous <>
2023-01-15 21:55:47 +0530

    updated api key

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore, collection} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtD5gFfO9Bwu4YXCgmZtpNQc9h2y1sjwY",
-  authDomain: "filmyverse-85261.firebaseapp.com",
-  projectId: "filmyverse-85261",
-  storageBucket: "filmyverse-85261.appspot.com",
-  messagingSenderId: "321991045980",
-  appId: "1:321991045980:web:10c03c5c285bdca6538799"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "filmy-4e1c5.firebaseapp.com",
+  projectId: "filmy-4e1c5",
+  storageBucket: "filmy-4e1c5.appspot.com",
+  messagingSenderId: "206563926562",
+  appId: "1:206563926562:web:cb0f036f3426455472e07b"
 };
 
 const app = initializeApp(firebaseConfig);
